7e19a1ab04d301d16b2c7fbbc8e4166535bb4f67
myles <myles@MYLES>
2023-06-13 11:52:49 -0400

    Added .NET 7

---
+++ EdgeTechWebAppNet7/Data/ApplicationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace EdgeTechWebApp.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ApplicationDBContext() : base("name=ApplicationDBContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<EdgeTechWebApp.Models.Review> Reviews { get; set; }
+    }
+}
