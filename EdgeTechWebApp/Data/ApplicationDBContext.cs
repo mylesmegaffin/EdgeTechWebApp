2433741e3719d588994d75d43c5b5d16009c0f48
myles <myles@MYLES>
2023-06-14 15:05:19 -0400

    Connected Azure Database

--- EdgeTechWebApp/Data/ApplicationDBContext.cs
+++ EdgeTechWebApp/Data/ApplicationDBContext.cs
@@ -15,7 +15,7 @@ namespace EdgeTechWebApp.Data
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
     
-        public ApplicationDBContext() : base("name=Reviews")
+        public ApplicationDBContext() : base("name=ApplicationDBContext")
         {
         }
 
